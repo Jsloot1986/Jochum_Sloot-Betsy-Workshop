a47e2a860063778c413eb042e2337d279637d9e5
Jochum Sloot <67738459+Jsloot1986@users.noreply.github.com>
2021-06-27 13:07:10 +0200

    update make_records()

--- models.py
+++ models.py
@@ -1,9 +1,9 @@
+from enum import unique
 from peewee import (CharField, Check, DateField, DecimalField,
                     ForeignKeyField, IntegerField, ManyToManyField, Model,
                     SqliteDatabase)
 
-database = SqliteDatabase('betsy_workshop.db', pragmas={"foreign_keys": 1})
-# When using sqlite pragma should be put on foreign_keys (only)
+database = SqliteDatabase('betsy_workshop.db')
 
 class BaseModel(Model):
     class Meta:
@@ -11,42 +11,35 @@ class BaseModel(Model):
 #if no primary key is defined an implicit primary key is added
 
 class User(BaseModel):
-    first_name = CharField()
+    first_name = CharField(unique=True)
     last_name = CharField()
     street = CharField()
-    town = CharField()
-
-class Tag(BaseModel):
-    name = CharField()
+    city = CharField()
 
 class Product(BaseModel):
     product_name = CharField()
     description = CharField()
-    price_per_unit = DecimalField(8, 2, True)
-    tags = ManyToManyField(Tag)
+    price_per_unit = DecimalField(decimal_places=2, auto_round=True)
+    owner = ForeignKeyField(User, backref='products')
 
-class UserProduct(BaseModel):
-    user_id = ForeignKeyField(User, backref='userproducts')
-    product_id = ForeignKeyField(Product, backref='product_ids')
-    number = IntegerField(constraints=[Check('number >=0')])
+class Tag(BaseModel):
+    name = CharField(unique=True)
+
+class ProductTags(BaseModel):
+    product = ForeignKeyField(Product)
+    tag = ForeignKeyField(Tag)
 
 class Transaction(BaseModel):
-    user_id = ForeignKeyField(User, backref='transactions')
+    buyer_id = ForeignKeyField(User, backref='transactions')
     product_id = ForeignKeyField(Product, backref='products')
     number = IntegerField(constraints=[Check('number>0')])
     sell_date = DateField()
     sell_price = DecimalField(8, 2, True)
 
-# class ProductTag(BaseModel):
-#     tag = ForeignKeyField(Tag, backref='product_tags')
-#     product = ForeignKeyField(Product, backref='product_tags')
-
-ProductTag = Product.tags.get_through_model()
-
 def create_tables():
     with database:
         database.create_tables(
-            [User, UserProduct, Product, Transaction, ProductTag, Tag]
+            [User, Product, Transaction]
         )
 
        
